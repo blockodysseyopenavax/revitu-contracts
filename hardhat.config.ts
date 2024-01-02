6c8ffb144109c33569c0e069a1bec083e3cd2079
blockodyssey-kmyoo <kmyoo@blockodyssey.io>
2024-01-02 17:32:20 +0900

    build: target london evm

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,14 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      // klaytn currently compatible with london
+      // https://docs.klaytn.foundation/docs/build/smart-contracts/porting-ethereum-contract/#solidity-support-
+      evmVersion: "london",
+    },
+  },
 };
 
 export default config;
