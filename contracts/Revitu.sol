5f296b237983d61bd0f302c4812820da6cfebf54
blockodyssey-kmyoo <kmyoo@blockodyssey.io>
2024-01-15 19:48:54 +0900

    feat: Revitu renewal

--- contracts/Revitu.sol
+++ contracts/Revitu.sol
@@ -1,75 +1,155 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@klaytn/contracts/KIP/token/KIP17/IKIP17Receiver.sol";
 
 contract Revitu is
     Initializable,
     UUPSUpgradeable,
-    ERC721Upgradeable,
-    ERC721URIStorageUpgradeable,
-    AccessControlUpgradeable
+    AccessControlUpgradeable,
+    ERC721EnumerableUpgradeable,
+    ERC721URIStorageUpgradeable
 {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    using Address for address;
+
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     uint256 private _nextTokenId;
-    string private baseTokenURI;
-
-    mapping(uint256 => bool) lockMap;
+    string private _baseTokenURI;
+    mapping(uint256 => bool) private _isLocked;
 
     uint256[47] private __gap;
 
-    /**
-     * @dev Disable implementation's initializer.
-     * See https://docs.openzeppelin.com/upgrades-plugins/1.x/writing-upgradeable#initializing_the_implementation_contract
-     */
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
-        _disableInitializers();
-    }
-
-    modifier transferValidator(uint256 _tokenId) {
-        require(lockMap[_tokenId], "this NFT is locked..");
-        _;
-    }
-
     function initialize(
-        address _defaultAdmin,
-        address _minter,
-        string memory _baseURI
+        address defaultAdmin,
+        address minter,
+        address upgrader,
+        string memory baseTokenURI
     ) public initializer {
-        __ERC721_init("Blockodyssey Revitu NFT", "REVITU");
-        __ERC721URIStorage_init();
-        __AccessControl_init();
         __UUPSUpgradeable_init();
+        __AccessControl_init();
+        __ERC721_init("Blockodyssey Revitu NFT", "REVITU");
+        __ERC721Enumerable_init();
+        __ERC721URIStorage_init();
 
-        _grantRole(DEFAULT_ADMIN_ROLE, _defaultAdmin);
-        _grantRole(MINTER_ROLE, _minter);
-        _grantRole(UPGRADER_ROLE, _defaultAdmin);
-        baseTokenURI = _baseURI;
+        _baseTokenURI = baseTokenURI;
+
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(MINTER_ROLE, minter);
+        _grantRole(UPGRADER_ROLE, upgrader);
     }
-
-    function _authorizeUpgrade(
-        address newImplementation
-    ) internal override onlyRole(UPGRADER_ROLE) {}
 
     function safeMint(
         address to,
-        string memory uri,
-        bool lockYn
+        string memory uri
     ) public onlyRole(MINTER_ROLE) {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
-        lockMap[tokenId] = lockYn;
     }
 
+    function setBaseTokenURI(
+        string memory baseTokenURI
+    ) public virtual onlyRole(DEFAULT_ADMIN_ROLE) {
+        _baseTokenURI = baseTokenURI;
+    }
+
+    function lockToken(uint256 tokenId) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _requireMinted(tokenId);
+        if (!_isLocked[tokenId]) _isLocked[tokenId] = true;
+    }
+
+    function unlockToken(uint256 tokenId) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _requireMinted(tokenId);
+        if (_isLocked[tokenId]) _isLocked[tokenId] = false;
+    }
+
+    function isLocked(uint256 tokenId) public view virtual returns (bool) {
+        _requireMinted(tokenId);
+        return _isLocked[tokenId];
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    )
+        public
+        view
+        virtual
+        override(
+            AccessControlUpgradeable,
+            ERC721EnumerableUpgradeable,
+            ERC721URIStorageUpgradeable
+        )
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    )
+        public
+        view
+        virtual
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function getBaseTokenURI() public view virtual returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 firstTokenId,
+        uint256 batchSize
+    )
+        internal
+        virtual
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
+    {
+        for (
+            uint256 tokenId = firstTokenId;
+            tokenId < firstTokenId + batchSize;
+            tokenId++
+        ) {
+            require(
+                !_isLocked[tokenId],
+                "Revitu: locked token cannot be moved"
+            );
+        }
+
+        super._beforeTokenTransfer(from, to, firstTokenId, batchSize);
+    }
+
+    // same function with KIP17._safeTransfer()
+    function _safeTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) internal virtual override {
+        _transfer(from, to, tokenId);
+        require(
+            _checkOnKIP17Received(from, to, tokenId, _data) ||
+                _checkOnERC721Received2(from, to, tokenId, _data),
+            "KIP17: transfer to non IKIP17Receiver/IERC721Receiver implementer"
+        );
+    }
+
+    // same function with KIP17._safeMint()
     function _safeMint(
         address to,
         uint256 tokenId,
@@ -83,103 +163,32 @@ contract Revitu is
         );
     }
 
-    function setLock(
-        uint256 _tokenId,
-        bool lockYn
-    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        lockMap[_tokenId] = lockYn;
-    }
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) public virtual override(ERC721Upgradeable, IERC721Upgradeable) {
-        _transfer(from, to, tokenId);
-    }
-
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) public virtual override(ERC721Upgradeable, IERC721Upgradeable) {
-        _safeTransfer(from, to, tokenId, "");
-    }
-
-    function _transfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal virtual override transferValidator(tokenId) {
-        super._transfer(from, to, tokenId);
-    }
-
-    function _safeTransfer(
-        address from,
-        address to,
-        uint256 tokenId,
-        bytes memory data
-    ) internal virtual override transferValidator(tokenId) {
-        super._transfer(from, to, tokenId);
-        require(
-            _checkOnKIP17Received(from, to, tokenId, data) ||
-                _checkOnERC721Received2(from, to, tokenId, data),
-            "ERC721: transfer to non ERC721Receiver implementer"
-        );
-    }
-
-    function tokenURI(
-        uint256 tokenId
-    )
-        public
-        view
-        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
-        returns (string memory)
-    {
-        require(_exists(tokenId), "Token does not exist");
-
-        string memory uri = baseTokenURI;
-        return
-            bytes(uri).length > 0
-                ? string(abi.encodePacked(uri, super.tokenURI(tokenId)))
-                : "";
-    }
-
-    function setBaseTokenUri(
-        string memory _baseURI
-    ) public onlyRole(MINTER_ROLE) {
-        baseTokenURI = _baseURI;
-    }
-
     function _burn(
         uint256 tokenId
-    ) internal override(ERC721Upgradeable, ERC721URIStorageUpgradeable) {
+    )
+        internal
+        virtual
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+    {
         super._burn(tokenId);
     }
 
-    function supportsInterface(
-        bytes4 interfaceId
-    )
-        public
-        view
-        virtual
-        override(
-            ERC721Upgradeable,
-            ERC721URIStorageUpgradeable,
-            AccessControlUpgradeable
-        )
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal virtual override onlyRole(UPGRADER_ROLE) {}
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
     }
 
+    // same function with KIP17._checkOnKIP17Received()
     function _checkOnKIP17Received(
         address from,
         address to,
         uint256 tokenId,
         bytes memory _data
     ) private returns (bool) {
-        if (to.code.length > 0) {
+        if (to.isContract()) {
             try
                 IKIP17Receiver(to).onKIP17Received(
                     _msgSender(),
@@ -203,13 +212,15 @@ contract Revitu is
         }
     }
 
+    // same function with ERC721._checkOnERC721Received()
+    // workaround for private non-virtual function ERC721._checkOnERC721Received()
     function _checkOnERC721Received2(
         address from,
         address to,
         uint256 tokenId,
         bytes memory data
     ) private returns (bool) {
-        if (to.code.length > 0) {
+        if (to.isContract()) {
             try
                 IERC721ReceiverUpgradeable(to).onERC721Received(
                     _msgSender(),
@@ -236,5 +247,14 @@ contract Revitu is
         } else {
             return true;
         }
+    }
+
+    /**
+     * @dev Disable implementation's initializer.
+     * See https://docs.openzeppelin.com/upgrades-plugins/1.x/writing-upgradeable#initializing_the_implementation_contract
+     */
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
     }
 }
