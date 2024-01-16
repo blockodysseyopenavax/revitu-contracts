f47600b08bf8142bf29204dfcd3b031f42086908
blockodyssey-kmyoo <kmyoo@blockodyssey.io>
2024-01-16 15:34:41 +0900

    chore: KIP17ERC721Holder

--- contracts/test/KIP17ERC721Holder.sol
+++ contracts/test/KIP17ERC721Holder.sol
@@ -1,4 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@klaytn/contracts/KIP/token/KIP17/utils/KIP17ERC721Holder.sol";
+import "./ERC721Holder.sol";
+import "./KIP17Holder.sol";
+
+contract KIP17ERC721Holder is ERC721Holder, KIP17Holder {}
